a0023611eb31279dd3a590bdf5ba9c65477e43dc
KMicheal70 <karimmicheal70@gmail.com>
2023-04-11 20:15:57 +0000

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,23 +11,30 @@ int main(void)
 	{
 		if (i % 5 == 0 && i % 3 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 			continue;
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 			continue;
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 			continue;
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
+		if (i == 100)
+		{
+			break;
+		}
+		printf(" ");
+		
 	}
+	printf("\n");
 	return (0);
 }
